3de980a452cd8e1ea23a22779dfc15cf3fdf6b6b
= <=>
2025-07-22 15:17:47 +0530

    test temp changes

--- drizzle.config.js
+++ drizzle.config.js
@@ -5,6 +5,6 @@ module.exports = defineConfig({
   out: "./src/drizzle/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: "postgresql://postgres:maggiatta123@db.peykhrcroyixquwxmbhe.supabase.co:5432/postgres",
+    url: "postgresql://postgres.peykhrcroyixquwxmbhe:maggiatta123@aws-0-ap-south-1.pooler.supabase.com:6543/postgres?sslmode=require",
   },
 });
